554fc16b913bb85f5831925ff75256ba428980f8
ShreyaReddy17 <43646675+ShreyaReddy17@users.noreply.github.com>
2022-08-29 20:52:36 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,20 @@
 #Calculator -- performs four basic operations - Addition, Subtraction, Multiplication and Division
   
-class Calculator():
-    
-    # ADD function
-    def add(num1, num2):
-        return num1 + num2
+  # ADD function
+def add(num1, num2):
+    return num1 + num2
   
   # SUBTRACT function
-    def subtract(num1, num2):
-        return num1 - num2
+def subtract(num1, num2):
+    return num1 - num2
   
   # MULTIPLY function
-    def multiply(num1, num2):
-        return num1 * num2
+def multiply(num1, num2):
+    return num1 * num2
   
   # DIVIDE function
-    def divide(num1, num2):
+def divide(num1, num2):
     # Check if num2 is 0 or not
-        if num2 == 0:
-            return "Denominator can't be zero"
-        return num1 / num2
+    if num2 == 0:
+        return "Denominator can't be zero"
+    return num1 / num2
