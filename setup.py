a8f5d54a770492afd26d5c27dbc373f390ec7f13
chetana-c <57283645+chetana-c@users.noreply.github.com>
2022-08-29 21:11:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6'
     ),
-    cmdclass={'test': pytest},
     tests_require=['pytest'],
     exclude_package_data={'': ['.gitignore'],
                             'images': ['*.xcf', '*.blend']},
