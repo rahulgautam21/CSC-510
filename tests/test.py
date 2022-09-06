a75da64fd1aa7ee725eab2f2208f85ad9aa81fe2
chetana-c <57283645+chetana-c@users.noreply.github.com>
2022-09-05 21:38:51 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -9,3 +9,15 @@ class TestsForLua(unittest.TestCase):
         dict = {'seperator':';'}
         the = cli(dict)
         self.assertEqual(the,dict)
+		
+	def sym()
+		sym = Sym()
+		list = ["a","a","a","a","b","b","c"]
+		for x in list:
+			sym.add(x)
+		mode = sym.mid()
+		entropy = sym.div()
+		entropy = Math.round(entropy,3)
+		print("mid="+mode)
+		print("div="+entropy)
+		self.assertEqual()
