ee76b899ed3064b1cdd29c005ccc901e89126210
srilekha98 <sri.agnes98@gmail.com>
2022-09-05 21:50:40 -0400

    added test for num

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,6 @@
 from src.main import *
+from src.sym import Sym
+from src.num import Num
 import unittest
 
 class TestsForLua(unittest.TestCase):
@@ -12,8 +14,8 @@ class TestsForLua(unittest.TestCase):
 		
 	def test_sym()
 		sym = Sym()
-		list = ["a","a","a","a","b","b","c"]
-		for x in list:
+		l = ["a","a","a","a","b","b","c"]
+		for x in l:
 			sym.add(x)
 		mode = sym.mid()
 		entropy = sym.div()
@@ -21,3 +23,14 @@ class TestsForLua(unittest.TestCase):
 		print("mid="+mode)
 		print("div="+entropy)
 		assert mode=="a" and 1.37 <= entropy and entropy <=1.38
+
+	def test_num()
+		sym = Num()
+		for i in range(100):
+			num.add(i+1)
+		mid = num.mid()
+		div = num.div()
+		print("mid:",mid)
+		print("div:",div)
+		assert mid>=50 and mid<=52
+		assert div>30.5 and div<32
